164670709176e40548555530266254c7a0a203af
Anicet Chiza <chizaanicet@gmail.com>
2024-11-11 19:47:18 +0200

    add to my repository

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -12,3 +12,33 @@ jQuery(function ($) {
         }
     });
 });
+
+/*------------------------------
+#Header
+/-----------------------------*/
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const list = document.querySelector('.head-list ul');
+    const menuIcon = document.querySelector('.menu-icon');
+    const exitIcon = document.querySelector('.exit-icon');
+    const overlay = document.querySelector('.overlay');
+    const menuItems = document.querySelectorAll('.header-list ul li a');
+
+    if(menuIcon){
+        menuIcon.addEventListener('click', () =>{
+            list.classList.add('active');
+            overlay.classList.add('active');
+            exitIcon.style.display = 'flex';
+            menuIcon.style.display = 'none';
+        })
+    }
+
+    if(exitIcon){
+        exitIcon.addEventListener('click', ( )=>{
+            list.classList.remove('active');
+            overlay.classList.remove('active');
+            exitIcon.style.display = 'none';
+            menuIcon.style.display = 'flex';
+        })
+    }
+});
